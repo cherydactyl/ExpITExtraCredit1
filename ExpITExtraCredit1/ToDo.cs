160255ef8fffcec72c8a7d9b90a5f1f8cd2d0f90
cherydactyl <cheryl.orosz@gmail.com>
2015-07-17 09:36:21 -0400

    Updated Constructors for ToDo
    Default constructor now creates and item to maintain the ToDo items,
    with a due date the day after creation.  Additional constructors added.

--- ExpITExtraCredit1/ToDo.cs
+++ ExpITExtraCredit1/ToDo.cs
@@ -8,5 +8,45 @@ namespace ExpITExtraCredit1
 {
     class ToDo
     {
+        string tag;         //a short/keyword description
+        string description; //longer description
+        DateTime dueDate;
+        DateTime creationDate;
+
+        ToDo()
+        {
+            //default ToDo item is by a reminder to maintain ToDo items/list
+            this.creationDate = DateTime.Now;
+            this.tag = "cleanup";
+            this.description = "maintenance and cleanup on to do items/list";
+            //Set the default to 1 day from Now
+            this.dueDate = DateTime.Now.AddYears(1);
+        }
+        ToDo(string d, DateTime due)
+        {
+            //default ToDo item is by a reminder to maintain ToDo items/list
+            this.creationDate = DateTime.Now;
+            this.tag = "";
+            this.description = d;
+            this.dueDate = due;
+        }
+        ToDo(string t, string d, DateTime due)
+        {
+            //default ToDo item is by a reminder to maintain ToDo items/list
+            this.creationDate = DateTime.Now;
+            this.tag = t;
+            this.description = d;
+            this.dueDate = due;
+        }
+        ToDo(string d)
+        {
+            //default ToDo item is by a reminder to maintain ToDo items/list
+            this.creationDate = DateTime.Now;
+            this.tag = "";
+            this.description = d;
+            //Set the default to 1 day from Now
+            this.dueDate = DateTime.Now.AddDays(1);
+        }
+
     }
 }
