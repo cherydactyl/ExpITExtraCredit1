41724c92b99acad08b7970381068111d88bf2b18
cherydactyl <cheryl.orosz@gmail.com>
2015-07-21 09:10:51 -0400

    ToDo class additions
    added several bool methods to test item state (status) eg, isDone().

--- ExpITExtraCredit1/ToDo.cs
+++ ExpITExtraCredit1/ToDo.cs
@@ -8,12 +8,14 @@ namespace ExpITExtraCredit1
 {
     class ToDo
     {
+        //define the list of states
         static Dictionary<int,string> states = new Dictionary <int,string> {{0, "To-Do"}, {1, "In Progress"}, {2,"Done"}};
+
         string tag { get; set; }            //a short/keyword description
         string description { get; set; }    //longer description
-        DateTime dueDate { get; set; }
-        DateTime creationDate;
-        int state { get; set; }
+        DateTime dueDate { get; set; }      //when the item is due to be completed by
+        DateTime creationDate;              //the automatically generated creation date of the item
+        int state { get; set; }             //stores the state key, as per the dictionary above
 
         ToDo()
         {
@@ -45,6 +47,7 @@ namespace ExpITExtraCredit1
             this.dueDate = due;
             this.state = 0;     //set item as "To-Do"
         }
+
         //contstructor takes one string (description) and sets due date to 1 day after creation
         ToDo(string d)
         {
@@ -57,36 +60,66 @@ namespace ExpITExtraCredit1
             this.state = 0;     //set item as "To-Do"
         }
 
+
         ToDo(string t, string d)
         {
-            //default ToDo item is by a reminder to maintain ToDo items/list
             this.creationDate = DateTime.Now;
             this.tag = t;
             this.description = d;
-            //Set the default to 1 day from Now
+            //Set the default to 1 day from Now, since it is not specified
             this.dueDate = DateTime.Now.AddDays(1);
             this.state = 0;     //set item as "To-Do"
         }
+
+        //looks up and returns the status as a string
         public string getState()
         {
             return states[this.state];      //returns the state by its name (string)
         }
+
+        //returns true if the status is To-Do, otherwise false
+        public bool isToDo()
+        {
+            return this.getState() == "To-Do";
+        }
+
+        //returns true if the status is InProgress, otherwise false
+        public bool isInProgress()
+        {
+            return this.getState() == "In Progress";
+        }
+
+        //returns true if the status is Done,. otherwise false
+        public bool isDone()
+        {
+            return this.getState() == "Done";
+        }
+
+        //returns the description
         public string getDescription()
         {
             return this.description;
         }
+
+        //returns the tag
         public string getTag()
         {
             return this.tag;
         }
+
+        //returns a DateTime object of the due date
         public DateTime getDueDate()
         {
             return this.dueDate;
         }
+
+        //returns a DateTime object of when the item was created
         public DateTime getCreationDate()
         {
             return this.creationDate;
         }
+
+        //
         public void print()
         {
             if (this.tag != "")
